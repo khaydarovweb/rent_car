5717b9f7e98288ba93d6c53a0f35b4eb113356e5
Abduvali <abduvalikhaydarov005@gmail.com@example.com>
2023-11-19 23:01:54 +0500

    backend worked

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import { ICars, Response } from "utils/types";
 import { cars } from "./cars";
 
 const app = express();
 
-app.use(cors)
+app.use(cors());
+app.use(express.json());
 
 const response: Response = {
-    message: '',
-    status: 200,
-    success: true,
-    data: []
-}
+  message: "",
+  status: 200,
+  success: true,
+  data: [],
+};
 
-app.get('/', (req, res) => {
-    response.message = "Success loading cars";
-    response.data = cars
+app.get("/", (req, res) => {
+  response.message = "Success loading cars";
+  response.data = cars;
 
-    return res.send(response);
-})
+  return res.send(response);
+});
 
 const PORT = 4000;
 
 app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`);
-})
+  console.log(`Listening on http://localhost:${PORT}`);
+});
